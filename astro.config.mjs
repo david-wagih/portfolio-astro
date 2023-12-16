501d03acbc2cababf447d06ef69c85829a0f21b7
David Wagih <78867146+david-wagih@users.noreply.github.com>
2023-12-16 15:08:25 +0200

    Update Astro configuration and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), mdx(), image()],
-});
+export default defineConfig({});
